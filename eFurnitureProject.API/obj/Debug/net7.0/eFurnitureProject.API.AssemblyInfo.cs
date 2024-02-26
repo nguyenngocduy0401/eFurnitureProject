2b77cc6babb32e196bfa68968304be1e47a9f761
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-02-26 13:50:20 +0700

    gitignore

--- eFurnitureProject.API/obj/Debug/net7.0/eFurnitureProject.API.AssemblyInfo.cs
+++ eFurnitureProject.API/obj/Debug/net7.0/eFurnitureProject.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eFurnitureProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fdac5ab7f4a80256858b4b60c00bb2ec6b908d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51145a13571ed4f2293e88ec4fd713ddfb102aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("eFurnitureProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eFurnitureProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

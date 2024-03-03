38a8dce6bec8147e55517b08f2feb746bc566b78
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-03-03 15:29:49 +0700

    Update

--- eFurnitureProject.Application/ViewModels/VoucherViewModels/UpdateVoucherDTO.cs
+++ eFurnitureProject.Application/ViewModels/VoucherViewModels/UpdateVoucherDTO.cs
@@ -12,7 +12,7 @@ namespace eFurnitureProject.Application.ViewModels.VoucherDTO
         public Guid Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public double Percent { get; set; 
+        public double Percent { get; set; } 
         public bool IsDeleted { get; set; }
     }
 }

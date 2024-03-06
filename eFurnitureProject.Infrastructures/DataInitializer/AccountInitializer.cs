3bba3ec46440afaae96458f755a8168fcf6b63b4
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-03-06 15:36:09 +0700

    Updateseed

--- eFurnitureProject.Infrastructures/DataInitializer/AccountInitializer.cs
+++ eFurnitureProject.Infrastructures/DataInitializer/AccountInitializer.cs
@@ -29,7 +29,7 @@ namespace eFurnitureProject.Infrastructures.DataInitializer
 
                 if (userRole == null)
                 {
-                    var newUser = await _userManager.CreateAsync(new User { UserName = role, PasswordHash = role });
+                    var newUser = await _userManager.CreateAsync(new User { UserName = role }, role);
 
                     if (newUser.Succeeded)
                     {

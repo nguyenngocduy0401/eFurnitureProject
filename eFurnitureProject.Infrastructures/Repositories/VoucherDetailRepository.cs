e948b45bac02f595a12ba0e3fcaafa440856fd82
tinhle2607 <lecongtinh223@gmail.com>
2024-03-10 18:06:47 +0700

    Merge branch 'Voucher'

--- eFurnitureProject.Infrastructures/Repositories/VoucherDetailRepository.cs
+++ eFurnitureProject.Infrastructures/Repositories/VoucherDetailRepository.cs
@@ -1,4 +1,6 @@
 ﻿using eFurnitureProject.Application.Repositories;
+using eFurnitureProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,16 @@ using System.Threading.Tasks;
 namespace eFurnitureProject.Infrastructures.Repositories
 {
     public class VoucherDetailRepository:IVoucherDetailRepository
+
     {
+        private readonly AppDbContext _dbContext;
+        public VoucherDetailRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(VoucherDetail voucherDetail)
+        {
+            await _dbContext.VouchersDetails.AddAsync(voucherDetail);
+        }
     }
 }

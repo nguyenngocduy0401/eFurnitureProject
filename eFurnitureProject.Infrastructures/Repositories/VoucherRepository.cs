0cad9e19e094c847681fdcad0975977e7fcff93f
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-03-13 02:51:10 +0700

    Fix Voucher

--- eFurnitureProject.Infrastructures/Repositories/VoucherRepository.cs
+++ eFurnitureProject.Infrastructures/Repositories/VoucherRepository.cs
@@ -19,6 +19,9 @@ namespace eFurnitureProject.Infrastructures.Repositories
             _dbContext = context;
         }
 
+        public async Task<bool> CheckVoucherNameExisted(string Name) =>
+           await _dbContext.Vouchers.AnyAsync(u => u.VoucherName == Name);
+
         public async Task<IEnumerable<Voucher>> Get(int pageIndex, int pageSize)
         {
             try

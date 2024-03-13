a815db2f6a698bb87996416e3b715dedb39c538f
hoangtlvse160649 <hoangtlvse160649@fpt.edu.vn>
2024-03-13 14:07:56 +0700

    fix : add field Pay into Contract.cs

--- eFurnitureProject.Domain/Entities/Contract.cs
+++ eFurnitureProject.Domain/Entities/Contract.cs
@@ -14,9 +14,10 @@ namespace eFurnitureProject.Domain.Entities
         public string? Description { get; set; }
         public int Value { get; set; }
         public int Status { get; set; } = 1;
+        public int Pay { get; set; }
         public Guid? OrderProcessingId { get; set; }
         [ForeignKey("OrderProcessingId")]
-        public OrderProcessing? OrderProcessing { get; set; }   
+        public OrderProcessing? OrderProcessing { get; set; }
 
     }
 }
--- eFurnitureProject.Infrastructures/Migrations/20240313070508_UpdateContract.Designer.cs
+++ eFurnitureProject.Infrastructures/Migrations/20240313070508_UpdateContract.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eFurnitureProject.Infrastructures;
 
@@ -11,9 +12,11 @@ using eFurnitureProject.Infrastructures;
 namespace eFurnitureProject.Infrastructures.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240313070508_UpdateContract")]
+    partial class UpdateContract
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

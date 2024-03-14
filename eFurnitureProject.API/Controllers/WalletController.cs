0c5d60a0e628a8f5ac5ae059e7984ad3f9da9e9f
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-03-14 14:27:41 +0700

    Update WalletController.cs

--- eFurnitureProject.API/Controllers/WalletController.cs
+++ eFurnitureProject.API/Controllers/WalletController.cs
@@ -14,11 +14,11 @@ namespace eFurnitureProject.API.Controllers
         {
             _walletService = walletService;
         }
-        /*[Authorize(Roles = AppRole.Admin)]*/
+        [Authorize(Roles = AppRole.Admin)]
         [HttpPost]
         public async Task<ApiResponse<string>> AddMoneyByUserIdAsync(UpdateWalletDTO updateWalletDTO) =>
             await _walletService.AddMoneyByUserIdAsync(updateWalletDTO);
-       /* [Authorize(Roles = AppRole.Admin)]*/
+        [Authorize(Roles = AppRole.Admin)]
         [HttpPost]
         public async Task<ApiResponse<string>> SubtractMoneyByUserIdAsync(UpdateWalletDTO updateWalletDTO) =>
             await _walletService.SubtractMoneyByUserIdAsync(updateWalletDTO);

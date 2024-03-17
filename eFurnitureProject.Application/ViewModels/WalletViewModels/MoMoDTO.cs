05c18c927754e022b488fad40fef27fee3c0614b
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-03-17 16:48:50 +0700

    Update MoMoDTO.cs

--- eFurnitureProject.Application/ViewModels/WalletViewModels/MoMoDTO.cs
+++ eFurnitureProject.Application/ViewModels/WalletViewModels/MoMoDTO.cs
@@ -9,7 +9,6 @@ namespace eFurnitureProject.Application.ViewModels.WalletViewModels
     public class MoMoDTO
     {
         public string signature { get; set; }
-        public string? ackTime { get; set; }
         public string tranId { get; set; }
         public string? partnerId { get; set; }
         public string? partnerName { get; set;}
